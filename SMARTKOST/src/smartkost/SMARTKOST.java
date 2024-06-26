9f5765c0a8fb07ea1655376cc4f74a481987456a
Aryawar004 <117187971+aryawar004@users.noreply.github.com>
2024-06-27 00:03:03 +0700

    Add files via upload

---
+++ SMARTKOST/src/smartkost/SMARTKOST.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package smartkost;
+
+/**
+ *
+ * @author user
+ */
+public class SMARTKOST {
+
+    /**
+     * @param args the command line arguments
+     */
+  public static void main(String[] args) {
+        // TODO code application logic here
+        FUtama FUtama=new FUtama ();
+        FUtama.setVisible(true);
+    }
+    
+}
